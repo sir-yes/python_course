a4a481f12a8f3bef5b6c4b768b21933d682717dc
sir-yes <hanthony2422@gmail.com>
2023-03-28 02:04:00 +0000

    Completed Caesar Cipher Program

--- Beginner/caesar_cipher.py
+++ Beginner/caesar_cipher.py
@@ -18,20 +18,28 @@ prime_checker(number=n)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(plain_text,shift_amount,cipher_direction):
     result = ""
     if cipher_direction == "decode":
         shift_amount *= - 1
     for i in plain_text:
-        get_index_of_i = alphabet.index(i)
-        #if cipher_direction == "decode":
-        #    shift_amount *= - 1
-        new_position = get_index_of_i + shift_amount
-        result += alphabet[new_position]
-        #shift_amount *= - 1
+        if i in alphabet:
+            get_index_of_i = alphabet.index(i)
+            new_position = get_index_of_i + shift_amount
+            result += alphabet[new_position]
+        else:
+            result += i
     print(f"The {cipher_direction}d text is {result}")
-caesar(text, shift, direction)
+
+looper = True
+while looper:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(text, shift, direction)
+    repeat = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if repeat == 'no':
+        looper = False
+        print("Goodbye")
+        
